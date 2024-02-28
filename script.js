32a4dd163ffcb2c7eec5946e1c99d14cf188d9c1
amrutha-b-nair <amruthabn00@gmail.com>
2024-02-28 15:48:32 +0100

    Update style

--- script.js
+++ script.js
@@ -1,20 +1,45 @@
-const colorcell = document.querySelectorAll('.cell');
-const gameGrid = document.getElementById('gameGrid');
+document.addEventListener('DOMContentLoaded', function() {
 
-for (let i = 0; i < 10; i++) {
-    for (let j = 0; j < 10; j++) {
-        const cell = document.createElement('div');
-        cell.classList.add('cell');
-        gameGrid.appendChild(cell);
+  const gameGrid = document.getElementById('gameGrid');
+  let gridContainer = document.querySelector('.game-grid');
+
+  let nrows = 10;
+  let celltot = nrows * nrows;
+
+  gridContainer.style.display = 'grid';
+  gridContainer.style.gridTemplateRows = `repeat(${nrows}, 1fr)`;
+  gridContainer.style.gridTemplateColumns = `repeat(${nrows}, 1fr)`;
+
+
+  let row = 1;
+  let column = 1;
+  for (let i = 1; i <= celltot; i++) {
+    let cell = document.createElement('div');
+    cell.classList.add('cell');
+    cell.style.gridRow = row;
+    cell.style.gridColumn = column;
+    column += 1;
+    if (column === nrows + 1) {
+      row += 1;
+      column = 1;
     }
-}
+    gridContainer.appendChild(cell);
+  }
 
+  console.log(gridContainer)
 
-colorcell.addEventListener('click', function() {
-    const currColor = colorcell.style.backgroundColor;
-    if (currColor === 'white' || currColor === '') {
-        colorcell.style.backgroundColor = "black";
-    } else {
-        colorcell.style.backgroundColor = "white";
-    }
+  const colorCells = document.querySelectorAll('.cell');
+  console.log(colorCells);
+
+  colorCells.forEach(cell => {
+    cell.addEventListener('click', function() {
+      const currColor = cell.style.backgroundColor;
+      if (currColor === 'white' || currColor === '') {
+        cell.style.backgroundColor = "black";
+      } else {
+        cell.style.backgroundColor = "white";
+      }
+    });
+  });
+
 });
